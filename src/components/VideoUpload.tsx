a93459f981c0123cc401f24e81ac3f2ddc4793ca
toshinori sato <tyahhahha@gmail.com>
2019-11-26 13:14:02 +0900

    upload処理の実装中

--- src/components/VideoUpload.tsx
+++ src/components/VideoUpload.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/firebase-storage";
 
 export const VideoUpload = () => {
-  return <div>Upload Video Here!</div>;
+  const [video, setVideo] = useState<File | null>(null);
+  return (
+    <form onSubmit={() => alert("todo on submit")}>
+      <h2>Video Upload</h2>
+      <input
+        type="file"
+        accept="video/*"
+        onChange={e => setVideo(e.target.files && e.target.files[0])}
+      />
+      <button type="submit">Upload Video</button>
+    </form>
+  );
 };
