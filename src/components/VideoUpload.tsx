93f2217f88678b3267b9d3fd1afe7a76330e82c8
cct-toshinori-sato <toshinori.sato@cct-inc.co.jp>
2019-11-26 13:36:35 +0900

    Upload処理の実装

--- src/components/VideoUpload.tsx
+++ src/components/VideoUpload.tsx
@@ -12,7 +12,34 @@ export const VideoUpload = () => {
         accept="video/*"
         onChange={e => setVideo(e.target.files && e.target.files[0])}
       />
-      <button type="submit">Upload Video</button>
+      <button
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          if (video) {
+            fileUpload(video);
+          }
+        }}
+      >
+        Upload Video
+      </button>
     </form>
   );
 };
+
+const fileUpload = async (video: File) => {
+  try {
+    const curentUser = firebase.auth().currentUser;
+    if (curentUser) {
+      const userUid = curentUser.uid;
+      const filePath = `videos/${userUid}/${video.name}`;
+      const videoStrageRef = firebase.storage().ref(filePath);
+      const fileSnapshot = await videoStrageRef.put(video);
+
+      console.log(fileSnapshot);
+    }
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
