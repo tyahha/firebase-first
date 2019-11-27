1fa18fd3f51cf4a751ca7c42c083970db4433d46
toshinori sato <tyahhahha@gmail.com>
2019-11-27 13:58:49 +0900

    videoのメタデータ更新処理の実装

--- src/components/VideoUpload.tsx
+++ src/components/VideoUpload.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import firebase from "firebase/app";
 import "firebase/firebase-storage";
+import "firebase/firestore";
+import _ from "lodash";
 
 export const VideoUpload = () => {
   const [video, setVideo] = useState<File | null>(null);
@@ -14,10 +16,13 @@ export const VideoUpload = () => {
       />
       <button
         type="submit"
-        onClick={e => {
+        onClick={async e => {
           e.preventDefault();
           if (video) {
-            fileUpload(video);
+            const isSuccess = await fileUpload(video);
+            if (!isSuccess) {
+              alert("ファイルのアップロードに失敗しました");
+            }
           }
         }}
       >
@@ -27,19 +32,55 @@ export const VideoUpload = () => {
   );
 };
 
-const fileUpload = async (video: File) => {
+const fileUpload = async (video: File): Promise<boolean> => {
   try {
-    const curentUser = firebase.auth().currentUser;
-    if (curentUser) {
-      const userUid = curentUser.uid;
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const userUid = currentUser.uid;
       const filePath = `videos/${userUid}/${video.name}`;
       const videoStrageRef = firebase.storage().ref(filePath);
-      const fileSnapshot = await videoStrageRef.put(video);
+      const idToken = await currentUser.getIdToken(true);
+      const metadataForStorage = {
+        customMetadata: {
+          idToken
+        }
+      };
+      const fileSnapshot = await videoStrageRef.put(video, metadataForStorage);
 
+      // mp4以外の動画は、Cloud Functions上で、トランスコードした後にメタデータをFirestoreに保存する
+      if (video.type === "video/mp4") {
+        const downloadURL = await videoStrageRef.getDownloadURL();
+        let metadataForFireStorage = _.omitBy(
+          fileSnapshot.metadata,
+          _.isEmpty
+        ) as firebase.storage.FullMetadata;
+        metadataForFireStorage = Object.assign(metadataForFireStorage, {
+          downloadURL
+        });
+        await saveVideoMetaData(userUid, metadataForFireStorage);
+      }
       console.log(fileSnapshot);
+      if (fileSnapshot.state === "success") {
+        return true;
+      }
     }
+    return false;
   } catch (error) {
     console.log(error);
-    return;
+    return false;
   }
-}
+};
+
+const saveVideoMetaData = async (
+  userUid: string,
+  metaData: firebase.storage.FullMetadata
+) => {
+  const videoRef = firebase
+    .firestore()
+    .doc(`users/${userUid}`)
+    .collection("videos")
+    .doc();
+  metaData = Object.assign(metaData, { uid: videoRef.id });
+
+  await videoRef.set(metaData, { merge: true });
+};
